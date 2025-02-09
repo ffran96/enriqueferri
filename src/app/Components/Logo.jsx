fae8cc71d1879d37e0a00c5b2f8d8452bab5055e
Fran <=>
2025-02-09 10:22:08 +0100

    Cambio de logo

--- src/app/Components/Logo.jsx
+++ src/app/Components/Logo.jsx
@@ -4,7 +4,7 @@ import Title1 from "./Title1";
 export default function Logo() {
   return (
     <div className="flex flex-row gap-1 text-[#ffffff]">
-      <Title1>Josselyn Castro</Title1>
+      <Title1>Enrique Ferri</Title1>
     </div>
   );
 }
