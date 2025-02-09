38b433bac46f8fac2958a8f071a199d159820016
Fran <=>
2025-02-09 10:31:03 +0100

    Se actualiza texto conoceme.

--- src/app/Components/Conoceme.jsx
+++ src/app/Components/Conoceme.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import ContentSection from "./ContentSection";
 import Underline from "./Underline";
 import Image from "next/image";
-import ConocemeImage from "../../../public/Enrique camara en mano.png"
+import ConocemeImage from "../../../public/Enrique camara en mano.png";
 import Title2 from "./Title2";
-
 
 export default function Conoceme() {
   return (
@@ -13,28 +12,24 @@ export default function Conoceme() {
         <div className="max-w-[600px] [&>p]:mb-4 [&>p]:leading-6 xl[&>p]:leading-[30px] [&>p]:text-lg md:[&>p]:text-2xl [&>p]:text-pretty">
           <Title2>Conóceme</Title2>
           <p>
-            Desde muy pequeña me he sentido atraída por el arte de la
-            interpretación. Siguiendo mi vocación me formé en la{" "}
-            <Underline>
-              Escuela Superior de Arte Dramático de Valencia,
-            </Underline>{" "}
-            estudios que he enriquecido con un entrenamiento actoral continuo en
-            el centro
-            <Underline> Atelier del Drama</Underline> y con talleres
-            especializados en lectura y análisis de textos, creación de
-            personajes e interpretación ante la cámara.
+            Desde siempre he sentido una profunda pasión por el mundo
+            audiovisual. De manera autodidacta, he perfeccionado mi estilo en la
+            realización de videoclips, combinando recursos visuales, efectos
+            dinámicos y una estética cuidada para crear experiencias que
+            impacten y conecten.
           </p>
           <p>
-            Estas experiencias me han proporcionado una sólida técnica actoral
-            al haber abordado personajes y textos complejos de autores como
-            <Underline> Chéjov, Strindberg y Tennessee Williams.</Underline> Mi
-            trayectoria incluye producciones teatrales, spots publicitarios,
-            videoclips y pequeños proyectos audiovisuales.
+            Mi inspiración proviene de la cultura urbana, sus sonidos y su
+            energía, elementos que integran cada uno de mis proyectos para
+            dotarlos de autenticidad y fuerza. Trabajo con artistas de diversos
+            géneros, buscando capturar la esencia de su música a través de una
+            narrativa visual potente y un montaje innovador.
           </p>
           <p className="italic">
-            &#34;Soy una actriz disciplinada, siempre hambrienta de conocimiento
-            y deseosa de encontrar proyectos en los que canalizar mi pasión por
-            este oficio y seguir creciendo como artista.&#34;
+            &#34;Soy un filmmaker comprometido con la creatividad, siempre en
+            busca de nuevos desafíos que me permitan evolucionar y seguir
+            contando historias a través de la imagen, el ritmo y la
+            emoción.&#34;
           </p>
         </div>
         <Image
