81341f0232ea32b214ee40e1bd558b2e65950eaf
Fran <=>
2025-02-09 12:25:15 +0100

    Cambio de foto en el conoceme

--- src/app/Components/Conoceme.jsx
+++ src/app/Components/Conoceme.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ContentSection from "./ContentSection";
 import Underline from "./Underline";
 import Image from "next/image";
-import ConocemeImage from "../../../public/Enrique en una grabación de videoclip.jpg";
+import ConocemeImage from "../../../public/Enrique camara en mano.png";
 import Title2 from "./Title2";
 
 export default function Conoceme() {
